0799449cb82f3bfcdf11fbf39ae8404c7d0322b3
Florian Cafiero <florian.cafiero@polytechnique.edu>
2025-07-03 22:57:11 +0200

    Update french_XX_XXI.py

--- french_XX_XXI.py
+++ french_XX_XXI.py
@@ -217,6 +217,151 @@ FUNCTION_WORDS: dict[str, frozenset[str]] = {
     "modals": MODAL_VERBS
 }
 
+
+ADV_LOCUTIONS = frozenset([
+    "tout de suite", "à nouveau", "de nouveau", "d'emblée", "en général",
+    "en particulier", "en fait", "en effet", "en réalité", "en somme",
+    "en outre", "de plus", "par contre", "au contraire", "au moins",
+    "à tout le moins", "c'est-à-dire", "bien sûr", "sans doute", "d'ailleurs",
+    "par ailleurs", "pour autant", "à ce propos", "à vrai dire",
+    "de toute façon", "quoi qu'il en soit", "quand même", "tout de même",
+    "pour l'instant", "pour le moment", "dans l'ensemble", "sur-le-champ",
+    "peu à peu", "au fur et à mesure", "dorénavant", "désormais",
+    "tout d'abord", "avant tout", "en premier lieu", "en dernier lieu",
+    "entre-temps", "jusqu'alors", "jusqu'ici", "tout de suite après",
+    "tout compte fait", "aujourd'hui même", "hier soir", "demain matin",
+    "tout récemment", "tout à coup", "d'une part", "d'autre part",
+    "en tout cas", "en tout temps", "en même temps", "pour ainsi dire",
+    "tant bien que mal", "tout au plus"
+])
+
+# ---------------------------------------------------------------------------
+# Flexions complètes des verbes modaux
+# ---------------------------------------------------------------------------
+def _forms(*cols: str) -> frozenset[str]:
+    """Helper pour lisibilité : _forms('peux', 'peut', ...) renvoie frozenset(...)."""
+    return frozenset(cols)
+
+MODAL_VERB_FORMS: dict[str, frozenset[str]] = {
+    # ---------------------------------------------------------------------
+    # POUVOIR
+    # ---------------------------------------------------------------------
+    "pouvoir": _forms(
+        # infinitif & participes
+        "pouvoir", "pouvant", "pu",
+        # indicatif présent
+        "peux", "peut", "pouvons", "pouvez", "peuvent",
+        # imparfait
+        "pouvais", "pouvait", "pouvions", "pouviez", "pouvaient",
+        # passé simple
+        "pus", "put", "pûmes", "pûtes", "purent",
+        # futur
+        "pourrai", "pourras", "pourra", "pourrons", "pourrez", "pourront",
+        # conditionnel
+        "pourrais", "pourrait", "pourrions", "pourriez", "pourraient",
+        # subjonctif présent
+        "puisse", "puisses", "puissions", "puissiez", "puissent",
+        # subjonctif imparfait
+        "pusse", "pusses", "pussions", "pussiez", "pussent"
+    ),
+    # ---------------------------------------------------------------------
+    # DEVOIR
+    # ---------------------------------------------------------------------
+    "devoir": _forms(
+        "devoir", "devant", "dû",
+        "dois", "doit", "devons", "devez", "doivent",
+        "devais", "devait", "devions", "deviez", "devaient",
+        "dus", "dut", "dûmes", "dûtes", "durent",
+        "devrai", "devras", "devra", "devrons", "devrez", "devront",
+        "devrais", "devrait", "devrions", "devriez", "devraient",
+        "doive", "doives", "devions", "deviez", "doivent",
+        "dusse", "dusses", "dussions", "dussiez", "dussent"
+    ),
+    # ---------------------------------------------------------------------
+    # VOULOIR
+    # ---------------------------------------------------------------------
+    "vouloir": _forms(
+        "vouloir", "voulant", "voulu",
+        "veux", "veut", "voulons", "voulez", "veulent",
+        "voulais", "voulait", "voulions", "vouliez", "voulaient",
+        "voulus", "voulu", "voulûmes", "voulûtes", "voulurent",
+        "voudrai", "voudras", "voudra", "voudrons", "voudrez", "voudront",
+        "voudrais", "voudrait", "voudrions", "voudriez", "voudraient",
+        "veuille", "veuilles", "veuillions", "veuilliez", "veuillent",
+        "voulusse", "voulusses", "voulussions", "voulussiez", "voulussent",
+        # impératif
+        "veuille", "veuillons", "veuillez"
+    ),
+    # ---------------------------------------------------------------------
+    # SAVOIR
+    # ---------------------------------------------------------------------
+    "savoir": _forms(
+        "savoir", "sachant", "su",
+        "sais", "sait", "savons", "savez", "savent",
+        "savais", "savait", "savions", "saviez", "savaient",
+        "sus", "sut", "sûmes", "sûtes", "surent",
+        "saurai", "sauras", "saura", "saurons", "saurez", "sauront",
+        "saurais", "saurait", "saurions", "sauriez", "sauraient",
+        "sache", "saches", "sachions", "sachiez", "sachent",
+        "susse", "susses", "sussions", "sussiez", "sussent",
+        "sache", "sachons", "sachez"
+    ),
+    # ---------------------------------------------------------------------
+    # FALLOIR (verbe impersonnel)
+    # ---------------------------------------------------------------------
+    "falloir": _forms(
+        "falloir", "fallu", "fallant",
+        "faut",           # présent
+        "fallait",        # imparfait
+        "fallut",         # passé simple
+        "faudra",         # futur
+        "faudrait",       # conditionnel
+        "faille",         # subjonctif présent
+        "fallût"          # subjonctif imparfait
+    ),
+    # ---------------------------------------------------------------------
+    # ALLER
+    # ---------------------------------------------------------------------
+    "aller": _forms(
+        "aller", "allant", "allé",
+        "vais", "vas", "va", "allons", "allez", "vont",
+        "allais", "allait", "allions", "alliez", "allaient",
+        "allai", "allas", "alla", "allâmes", "allâtes", "allèrent",
+        "irai", "iras", "ira", "irons", "irez", "iront",
+        "irais", "irait", "irions", "iriez", "iraient",
+        "aille", "ailles", "aille", "allions", "alliez", "aillent",
+        "allasse", "allasses", "allât", "allassions", "allassiez", "allassent",
+        "va", "allons", "allez"   # impératif
+    ),
+    # ---------------------------------------------------------------------
+    # VENIR
+    # ---------------------------------------------------------------------
+    "venir": _forms(
+        "venir", "venant", "venu",
+        "viens", "vient", "venons", "venez", "viennent",
+        "venais", "venait", "venions", "veniez", "venaient",
+        "vins", "vint", "vînmes", "vîntes", "vinrent",
+        "viendrai", "viendras", "viendra", "viendrons", "viendrez", "viendront",
+        "viendrais", "viendrait", "viendrions", "viendriez", "viendraient",
+        "vienne", "viennes", "vienne", "venions", "veniez", "viennent",
+        "vinsse", "vinsses", "vînt", "vinssions", "vinssiez", "vinssent",
+        "viens", "venons", "venez"
+    )
+}
+
+# Union plate de toutes les formes modales
+MODALS_FULL = frozenset().union(*MODAL_VERB_FORMS.values())
+
+# ---------------------------------------------------------------------------
+# Ajout au master dict
+# ---------------------------------------------------------------------------
+FUNCTION_WORDS.update({
+    "adv_locutions": ADV_LOCUTIONS,
+    "modals_full": MODALS_FULL   # pour filtrage brut
+})
+
+
+
 # ---------------------------------------------------------------------------
 # Convenience flat set
 # ---------------------------------------------------------------------------
