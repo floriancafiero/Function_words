9c9eaa2f1ba40e7eea7cb990482a8ec534d2efec
Florian Cafiero <florian.cafiero@polytechnique.edu>
2025-07-04 16:04:13 +0200

    Update test_loader.py

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,8 +1,16 @@
 import functionwords as fw
 
-print("Jeux dispo :", fw.available_ids())
-fr = fw.load("fr_21c")
-print(len(fr.all), "mots outils - français contemporain")
-print("'ne' in set ?", 'ne' in fr.all)
-subset = fr.subset(["articles", "prepositions"])
-print("articles+prépositions :", len(subset))
+def test_loader_core():
+    # 1. on trouve le jeu fr_21c
+    ids = fw.available_ids()
+    assert "fr_21c" in ids and ids, "fr_21c should be listed"
+
+    fr = fw.load("fr_21c")
+    # 2. taille approximative connue
+    assert 580 < len(fr.all) < 700
+    # 3. contrôle de présence
+    assert "ne" in fr.all
+    # 4. subset sur 2 catégories
+    sub = fr.subset(["articles", "prepositions"])
+    assert sub                               # not empty
+    assert sub.issubset(fr.all)
