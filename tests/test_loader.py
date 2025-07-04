acaf80e90a1ecf7bdf7be901c2911ad05840cf92
Florian Cafiero <florian.cafiero@polytechnique.edu>
2025-07-04 16:01:25 +0200

    Update test_loader.py

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,7 +2,7 @@ import functionwords as fw
 
 print("Jeux dispo :", fw.available_ids())
 fr = fw.load("fr_21c")
-print(len(fr.all), "mots français modernes")
+print(len(fr.all), "mots outils - français contemporain")
 print("'ne' in set ?", 'ne' in fr.all)
 subset = fr.subset(["articles", "prepositions"])
 print("articles+prépositions :", len(subset))
